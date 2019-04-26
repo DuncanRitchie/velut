25480a887e56e0412fe1314e1e0046e83dae547b
Duncan Ritchie <duncanritchie@btinternet.com>
2019-04-26 16:44:18 +0100

    Moved S3T's query code from App to a separate file

---
+++ src/examplequery.js
@@ -0,0 +1,34 @@
+// // This is the node.js code for a Mongo query
+
+let mongodb = require('mongodb');
+let client = mongodb.MongoClient;
+let url = "mongodb://host:port/";
+
+client.connect(url, function (err, client) {
+    
+    let db = client.db("local");
+    let collection = db.collection("words");
+    
+    let query = {
+        "Scansion": "\u02D8\u02D8\u00AF\u02D8"
+    };
+    
+    let projection = {
+        "Word": 1.0
+    };
+    
+    let sort = [ ["Sort column", 1] ];
+    let skip = 116;
+    let limit = 50;
+    
+    let cursor = collection.find(query).project(projection).sort(sort).skip(skip).limit(limit);
+    
+    cursor.forEach(
+        function(doc) {
+            console.log(doc);
+        }, 
+        function(err) {
+            client.close();
+        }
+    );
+});
