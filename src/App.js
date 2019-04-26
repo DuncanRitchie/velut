ed07ef4ddbaf9f87efaae18da0424759dda6a9a7
Duncan Ritchie <duncanritchie@btinternet.com>
2019-04-26 15:56:20 +0100

    Renamed words.json to words_batch1.json

--- src/App.js
+++ src/App.js
@@ -2,24 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// // This is the node.js code for a Mongo query
+
+// let mongodb = require("mongodb");
+
+// let client = mongodb.MongoClient;
+// let url = "mongodb://host:port/";
+
+// client.connect(url, function (err, client) {
+    
+//     let db = client.db("local");
+//     let collection = db.collection("words");
+    
+//     let query = {
+//         "Scansion": "\u02D8\u02D8\u00AF\u02D8"
+//     };
+    
+//     let projection = {
+//         "Word": 1.0
+//     };
+    
+//     let sort = [ ["Sort column", 1] ];
+//     let skip = 116;
+//     let limit = 50;
+    
+//     let cursor = collection.find(query).project(projection).sort(sort).skip(skip).limit(limit);
+    
+//     cursor.forEach(
+//         function(doc) {
+//             console.log(doc);
+//         }, 
+//         function(err) {
+//             client.close();
+//         }
+//     );
+// });
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        There&rsquo;s nothing here yet&hellip;
       </div>
     );
   }
