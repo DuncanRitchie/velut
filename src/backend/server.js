121845d31f06c69016f51f32c056aca2a9adb8b2
DuncanRitchie <duncanritchie@btinternet.com>
2019-04-29 12:05:56 +0100

    Continued to try to get Atlas working

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 4000;
+
+const key = "4a3ae2cc-4eb3-4ffc-bf26-927b92ae2080"
+
 app.use(cors());
 app.use(bodyParser.json());
 app.listen(PORT, function() {
