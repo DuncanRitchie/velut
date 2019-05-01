8c230c2d5e4f1e32968ce3b2dd0a5f084b75bdfe
DuncanRitchie <duncanritchie@btinternet.com>
2019-05-01 16:49:45 +0100

    Perfect rhymes now come through nicely

--- src/components/Lemma.jsx
+++ src/components/Lemma.jsx
@@ -6,7 +6,7 @@ let Lemma = (props) => {
             <h2>{props.lemma}</h2>
             <p>Part of speech: {props.partOfSpeech.toLowerCase()}</p>
             <p>Meaning: {props.meaning}</p>
-            {props.scansion ? <p>Scansion: {props.scansion}</p> : null}
+            {props.scansion ? <p>Scansion of lemma: {props.scansion}</p> : null}
             <p>Cognates: {props.cognatesMessage} {props.cognates}</p>
         </div>
     )
--- src/components/WordFromJson.jsx
+++ src/components/WordFromJson.jsx
@@ -9,12 +9,40 @@ let WordFromJson = (props) => {
     // The word searched for comes from the window location.
     let input = window.location.pathname.replace("/word","").replace("/","");
     document.title = input+" on velut"
-    // foundWord is the first object that matches the input (macron-insensitive but case-sensitive).
-    let foundWord = words.find(word=>{return word["No macra"]===input})
+    // foundWord is the first object that matches the input.
+    // It looks for an exact match, then ignores macra and looks again, then ignores case and looks again.
+    let foundWord = words.find(word=>{return word.Word===input})
+    if (!foundWord) {
+        foundWord = words.find(word=>{return word["No macra"]===input})
+    }
+    if (!foundWord) {
+        foundWord = words.find(word=>{return word["No macra"].toLowerCase()===input.toLowerCase()})
+    }
+    let mappedRhymes = []
     let wordLemmata = [];
     let mappedLemmata = [];
     if (foundWord) {
-        // We will render an element for every lemma listed against the input.
+        // Let's find the rhymes.
+        let rhymes = words.filter((word)=>{return word["Perfect rhyme"]===foundWord["Perfect rhyme"]})
+        // The rhymes get sorted by syllable count, then by Sort Column.
+        let sortedRhymes = rhymes.sort((a,b)=>{
+            if (a["Syllable count"]===b["Syllable count"]) {
+                if (a["Sort column"]>b["Sort column"]) {
+                return 1
+            }
+            else {
+                return -1
+            }
+            }
+            else {
+                return a["Syllable count"]-b["Syllable count"]
+            }
+        })
+        // A react-router-dom Link is rendered for every rhyme.
+        mappedRhymes = sortedRhymes.map((rhyme,index)=>{return (
+            <Link to={"/"+rhyme["No macra"]}>{rhyme.Word} </Link>
+        )})
+        // Let's do the lemmata. We will render an element for every lemma listed against the input.
         wordLemmata = foundWord.Lemmata.split(" ")
         mappedLemmata = wordLemmata.map((lemma,index)=>{ 
             // Let's find the lemma in the Json.
@@ -37,7 +65,7 @@ let WordFromJson = (props) => {
             });
             // A react-router-dom Link is rendered for every cognate.
             let mappedCognates = sortedCognates.map((cognate,index)=>{
-                return <Link to={`/lemma/${cognate["No Macra"]}`} key={index}> {cognate.Lemma} </Link>
+                return <span><Link to={`/${cognate["No Macra"]}`} key={index}> {cognate.Lemma}</Link> </span>
             })
             // Cognates are done. Let's put everything into the Lemma element.
             return (
@@ -59,7 +87,11 @@ let WordFromJson = (props) => {
             <p>Welcome to my Useful Tables of Excellent Latin Vocabulary!</p>
             <p>I&rsquo;m still in the initial stages of creating this app&hellip;</p>
             <Search/>
-            <p>You searched for the word <strong>{input}</strong>. {foundWord ? <span>The word <strong>{foundWord.Word}</strong> belongs to the following {wordLemmata.length} {wordLemmata.length===1 ? "lemma" : "lemmata"}:</span> : <span>Nothing was found.</span>}</p>
+            <p>You searched for the word <strong>{input}</strong>.</p>
+            {foundWord ? <div><p>The word <strong>{foundWord.Word}</strong> could scan as {foundWord.Scansion}</p>
+            <p>Perfect rhymes: {mappedRhymes}</p>
+            <div className="divider"/>
+            <p>{foundWord.Word} belongs to the following {wordLemmata.length} {wordLemmata.length===1 ? "lemma" : "lemmata"}:</p></div> : <p>Nothing was found.</p>}
             {mappedLemmata ? mappedLemmata : null}
         </div>
     )
