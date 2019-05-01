2d4547c04439f025e1af9334724075d039acdd6c
DuncanRitchie <duncanritchie@btinternet.com>
2019-05-01 13:28:57 +0100

    Started making WordFromJson

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Home from "./components/Home";
+import WordFromJson from "./components/WordFromJson";
 import Lemmata from "./components/Lemmata";
 import Footer from "./components/Footer";
 import './App.css';
@@ -10,8 +11,8 @@ class App extends Component {
     return (
       <Router>
         <Route path="/" exact component={Home} />
-        {/* <Route path="/word/:word" component={Word} /> */}
-        <Route path="/:lemma" component={Lemmata}/>
+        <Route path="/:lemma" exact component={Lemmata} />
+        <Route path="/word/:word" component={WordFromJson} />
         <Footer />
       </Router>
     );
---
+++ src/components/WordFromJson.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import Search from "./Search";
+import lemmata from "../data/lemmata.json";
+import Lemma from "./Lemma"
+
+let WordFromJson = (props) => {
+    return (
+        <div>
+            <p>Word from Json</p>
+        </div>
+    )
+}
+
+export default WordFromJson
