f2fc55677acc348b36cc56f22167ccae6bb210db
DuncanRitchie <duncanritchie@btinternet.com>
2019-05-03 17:45:52 +0100

    I still need to make client in stitch.js mean something

--- src/stitch.js
+++ src/stitch.js
@@ -1,21 +1,56 @@
 const {
     Stitch, 
     AnonymousCredential,
+    RemoteMongoClient
 } = require('mongodb-stitch-server-sdk');
 
-let client = Stitch.initializeDefaultAppClient('velutweb-yybap');
-client.auth.loginWithCredential(new AnonymousCredential()).then(user => {
+let stitchClient = Stitch.initializeDefaultAppClient('velutweb-yybap');
+// client is changed to .defaultAppClient later
+stitchClient.auth.loginWithCredential(new AnonymousCredential()).then(user => {
     console.log(user);
-    client.close();
+    stitchClient.close();
 }).catch(err => {
     console.log(err);
-    client.close();
+    stitchClient.close();
 })
 
-let client = Stitch.defaultAppClient;
+stitchClient = Stitch.defaultAppClient;
 
 console.log("logging in anonymously");
-client.auth.loginWithCredential(new AnonymousCredential()).then(user => {
+stitchClient.auth.loginWithCredential(new AnonymousCredential()).then(user => {
   console.log(`logged in anonymously as user ${user.id}`)
 });
 
+// const client = stitchClient.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas')
+const client = stitchClient
+
+let url = "mongodb+srv://duncanritchie:Computatrum%5F2019@cluster0-nmpwr.azure.mongodb.net/test?retryWrites=true"
+
+// Query Mongo for the perfect rhyme ending of gallīna.
+
+// I need to find out what client should be.
+client.connect(url, function (err, client) {
+    
+  const db = client.db("velut");
+  const collection = db.collection("words");
+  
+  let query = {
+      "Word": "gall\u012Bna"
+  };
+  
+  let projection = {
+      "Perfect rhyme": 1.0
+  };
+  
+  let cursor = collection.find(query).project(projection);
+  
+  cursor.forEach(
+      function(doc) {
+          console.log(doc);
+      }, 
+      function(err) {
+          client.close();
+      }
+  );
+  
+});
