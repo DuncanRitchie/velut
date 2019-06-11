cef7f6292fe8835e538cdd4cdb50a3a1fc15e74c
Duncan Ritchie <duncanritchie@btinternet.com>
2019-06-11 15:13:31 +0100

    Added mongoexport.json files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,13 @@ import WarningMessage from './WarningMessage';
 import Search from './Search';
 
 let Home = () => {
-    document.title = "Duncan Ritchie's velut"
+    document.title = "Duncan Ritchie’s velut"
     return (
         <div className="home">
             <h1><span className="title">velut</span> &mdash; Vocābulōrum Excellentium Latīnōrum Ūtilēs Tabulae</h1>
             <Navbar input="" currentPage="word"/>
             <p>Welcome to my Useful Tables of Excellent Latin Vocabulary!</p>
-            <p>Search for a Latin word!</p>
+            <p>Search for a Latin word using the searchbar below!</p>
             <WarningMessage />
             <Search prefix="" />
         </div>
