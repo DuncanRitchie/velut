bca78968491dea11a9d401207bf2910ab4758517
Duncan Ritchie <duncanritchie@btinternet.com>
2019-06-20 13:16:42 +0100

    Undid last commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "./components/Home";
-// import WordFromJson from "./components/WordFromJson";
-// import Lemmata from "./components/Lemmata";
-// import CountdownHome from "./components/CountdownHome";
-// import Countdown from "./components/Countdown";
-// import AnagramsHome from "./components/AnagramsHome";
-// import Anagrams from "./components/Anagrams";
+import WordFromJson from "./components/WordFromJson";
+import Lemmata from "./components/Lemmata";
+import CountdownHome from "./components/CountdownHome";
+import Countdown from "./components/Countdown";
+import AnagramsHome from "./components/AnagramsHome";
+import Anagrams from "./components/Anagrams";
 import Footer from "./components/Footer";
 import './App.css';
 
@@ -16,14 +16,13 @@ class App extends Component {
       <div className="App">
         <Router>
           <Switch>
-            {/* <Route path="/" exact component={Home} />
+            <Route path="/" exact component={Home} />
             <Route path="/lemma/:word" component={Lemmata} />
             <Route path="/countdown/:word" component={Countdown} />
             <Route path="/countdown" component={CountdownHome} />
             <Route path="/anagrams/:word" component={Anagrams} />
             <Route path="/anagrams" component={AnagramsHome} />
-            <Route path="/:word" component={WordFromJson} /> */}
-            <Route path="/" component={Home} />
+            <Route path="/:word" component={WordFromJson} />
           </Switch>
           <Footer />
         </Router>
