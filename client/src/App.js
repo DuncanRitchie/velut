378aa39585878ffa6f2cda7d5883ade2880be072
Duncan Ritchie <duncanritchie@btinternet.com>
2019-07-22 13:36:38 +0100

    Renamed SubwordsHome in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "./components/word/Home";
 import WordFromJson from "./components/word/Word";
-import CountdownHome from "./components/subwords/SubwordsHome";
+import SubwordsHome from "./components/subwords/SubwordsHome";
 import Subwords from "./components/subwords/Subwords";
 // import AnagramsHome from "./components/anagrams/AnagramsHome";
 // import Anagrams from "./components/anagrams/Anagrams";
@@ -18,7 +18,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/subwords/:word" component={Subwords}/>
-            <Route path="/subwords" component={CountdownHome} />
+            <Route path="/subwords" component={SubwordsHome} />
             {/* <Route path="/anagrams/:word" component={Anagrams} />
             <Route path="/anagrams" component={AnagramsHome} /> */}
             <Route path="/about" component={About} />
