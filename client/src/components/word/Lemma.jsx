35a0de10d9e29b85cdeacec9af6ca2402f4b9a27
DuncanRitchie <duncanritchie@btinternet.com>
2019-08-12 12:33:30 +0100

    Added notes & transliteration to Lemma

--- client/src/components/word/Lemma.jsx
+++ client/src/components/word/Lemma.jsx
@@ -6,6 +6,8 @@ let Lemma = (props) => {
             <h3 lang="la">{props.lemma}</h3>
             {props.partOfSpeech ? <p>Part of speech: {props.partOfSpeech.toLowerCase()}</p> : null}
             {props.meaning ? <p>Meaning: {props.meaning}</p> : null}
+            {props.notes ? <p>Notes: {props.notes}</p> : null}
+            {props.transliteration ? <p>Transliteration: {props.transliteration}</p> : null}
             {props.forms ? <p>Forms: {props.forms}</p> : null}
             <p>{props.cognatesMessage ? props.cognatesMessage : <span>Cognates: {props.cognates}</span>}</p>
         </div>
