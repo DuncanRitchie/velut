9cbbd34f8e2cc68779f10e3e7315441c076931d4
DuncanRitchie <duncanritchie@btinternet.com>
2019-08-12 13:49:35 +0100

    Transliterations are assigned individual langs

--- client/src/components/word/Lemma.jsx
+++ client/src/components/word/Lemma.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
 let Lemma = (props) => {
+    let transliterations
+    if (props.transliteration) {
+        // props.transliteration could look like "Ἰησοῦς/יֵשׁוּעַ"
+        let transliterationsArray = props.transliteration.split("/")
+        transliterations = transliterationsArray.map((word,i)=>{
+            let langCode = "he";
+            let emoji = "🇮🇱"
+            if("αβγδεζηθικλμνξοπρςτυφχψω".includes(word.substr(-1).toLowerCase())
+             || "αβγδεζηθικλμνξοπρςτυφχψω".includes(word.substr(-2,1).toLowerCase())
+             ) {
+                langCode = "grc"
+                emoji = "🇬🇷"
+            }
+            return <span key={i} lang={langCode}>{emoji} {word} </span>
+        })
+    }
+    
     return (
         <div className="lemma">
             <h3 lang="la">{props.lemma}</h3>
             {props.partOfSpeech ? <p>Part of speech: {props.partOfSpeech.toLowerCase()}</p> : null}
             {props.meaning ? <p>Meaning: {props.meaning}</p> : null}
             {props.notes ? <p>Notes: {props.notes}</p> : null}
-            {props.transliteration ? <p>Transliteration: <span lang="grc">{props.transliteration}</span></p> : null}
+            {transliterations ? <p>Transliteration: {transliterations}</p> : null}
             {props.forms ? <p>Forms: {props.forms}</p> : null}
             <p>{props.cognatesMessage ? props.cognatesMessage : <span>Cognates: {props.cognates}</span>}</p>
         </div>
