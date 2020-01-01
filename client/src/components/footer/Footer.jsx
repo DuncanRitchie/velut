378497a27b7242e9694c08143e4b8ab06627fcf9
DuncanRitchie <duncanritchie@btinternet.com>
2020-01-01 22:48:02 +0000

    Footer link to English

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -12,18 +12,27 @@ let Footer = (props) => {
                         &copy; Duncan Ritchie
                     </span>
                 </li>
+                
                 {props.history.length>2
                     ? (<li><button className="go-back" tabIndex="0" onClick={()=>{props.history.goBack()}} title="Go back to the previous page">Back</button></li>)
                     : null}
+
                 {props.history.location.pathname==="/"
                     ? null 
                     : <li><Link to="/" title="velut homepage"><span>Home</span></Link></li> }
+
+                {props.history.location.pathname.substr(0,8)==="/english"
+                    ? null 
+                    : <li><Link to="/english" title="English to Latin on velut"><span>English to Latin</span></Link></li> }
+
                 {props.history.location.pathname.substr(0,9)==="/subwords"
-                        ? null 
-                        : <li><Link to="/subwords" title="Subwords on velut"><span>Subwords</span></Link></li> }
+                    ? null 
+                    : <li><Link to="/subwords" title="Subwords on velut"><span>Subwords</span></Link></li> }
+
                 {props.history.location.pathname==="/about" || props.history.location.pathname==="/about/"
                     ? null 
                     : <li><Link to="/about" title="About velut"><span>About velut</span></Link></li> }
+
                 <li>
                     <a href="https://www.duncanritchie.co.uk" title="Duncan Ritchie&rsquo;s website"><span>My website</span></a>
                 </li>
