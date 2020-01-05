342e802b6256119743a34fedd1710a4690c051a3
DuncanRitchie <duncanritchie@btinternet.com>
2020-01-05 21:42:10 +0000

    Footer doesn’t spill to 2 lines if viewport width>762px

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,8 +4,15 @@ import "../../App.css"
 import "./Footer.css"
 
 let Footer = (props) => {
+    let showBack = props.history.length > 2
+    let showHome = props.history.location.pathname !== "/"
+    let showEnglish = props.history.location.pathname.substr(0,8) !== "/english"
+    let showSubwords = props.history.location.pathname.substr(0,9) !== "/subwords"
+    let showAbout = props.history.location.pathname !== "/about" && props.history.location.pathname !== "/about/"
+    let countListItems = 2 + showBack + showHome + showEnglish + showSubwords + showAbout
+
     return (
-        <footer className="footer">
+        <footer className={"footer" + " footerContaining" + countListItems}>
             <ul>
                 <li>
                     <span className="footer-copyright">
@@ -13,29 +20,17 @@ let Footer = (props) => {
                     </span>
                 </li>
                 
-                {props.history.length>2
-                    ? (<li><button className="go-back" tabIndex="0" onClick={()=>{props.history.goBack()}} title="Go back to the previous page">Back</button></li>)
-                    : null}
+                {showBack && <li><button className="go-back" tabIndex="0" onClick={()=>{props.history.goBack()}} title="Go back to the previous page">Back</button></li>}
 
-                {props.history.location.pathname==="/"
-                    ? null 
-                    : <li><Link to="/" title="velut homepage"><span>Home</span></Link></li> }
+                {showHome && <li><Link to="/" title="velut homepage"><span>Home</span></Link></li>}
 
-                {props.history.location.pathname.substr(0,8)==="/english"
-                    ? null 
-                    : <li><Link to="/english" title="English to Latin on velut"><span>English to Latin</span></Link></li> }
+                {showEnglish && <li><Link to="/english" title="English to Latin on velut"><span>English to Latin</span></Link></li>}
 
-                {props.history.location.pathname.substr(0,9)==="/subwords"
-                    ? null 
-                    : <li><Link to="/subwords" title="Subwords on velut"><span>Subwords</span></Link></li> }
+                {showSubwords && <li><Link to="/subwords" title="Subwords on velut"><span>Subwords</span></Link></li>}
 
-                {props.history.location.pathname==="/about" || props.history.location.pathname==="/about/"
-                    ? null 
-                    : <li><Link to="/about" title="About velut"><span>About velut</span></Link></li> }
+                {showAbout && <li><Link to="/about" title="About velut"><span>About velut</span></Link></li>}
 
-                <li>
-                    <a href="https://www.duncanritchie.co.uk" title="Duncan Ritchie&rsquo;s website"><span>My website</span></a>
-                </li>
+                <li><a href="https://www.duncanritchie.co.uk" title="Duncan Ritchie&rsquo;s website"><span>My website</span></a></li>
             </ul>
         </footer>
     )
