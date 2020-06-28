b850f2a23de889c17a8f5bf2e2dd0bacd11a0763
DuncanRitchie <duncanritchie@btinternet.com>
2020-06-28 23:45:24 +0100

    Finished countLetters.js & updated letterCounts.json

--- client/src/components/word/macraToHyphens.js
+++ client/src/components/word/macraToHyphens.js
@@ -4,7 +4,7 @@ const macraToHyphens = (macronizedWord) => {
     // Acutes are converted to dots.
     let toDots = toHyphens.replace(/\u00C1/g,"A.").replace(/\u00C9/g,"E.").replace(/\u00CD/g,"I.").replace(/\u00D3/g,"O.").replace(/\u00DA/g,"U.").replace(/\u00DD/g,"Y.").replace(/\u00E1/g,"a.").replace(/\u00E9/g,"e.").replace(/\u00ED/g,"i.").replace(/\u00F3/g,"o.").replace(/\u00FA/g,"u.").replace(/\u00FD/g,"y.")
     // Diaereses are converted to cola.
-    toDots = toDots.replace(/\u00E4/g,"a:").replace(/\u00EB/g,"e:").replace(/\u00EF/g,"i:").replace(/\u00F6/g,"o:").replace(/\u00FC/g,"u:").replace(/\u00FF/g,"y:")
+    toDots = toDots.replace(/\u00E4/g,"a:").replace(/\u00EB/g,"e:").replace(/\u00CF/g,"i:").replace(/\u00EF/g,"i:").replace(/\u00F6/g,"o:").replace(/\u00FC/g,"u:").replace(/\u00FF/g,"y:")
     return toDots
 }
 
