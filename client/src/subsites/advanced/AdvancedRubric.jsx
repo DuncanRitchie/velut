3cfc109e42174cef6f7d87bb5621e0c5822c926a
DuncanRitchie <duncanritchie@btinternet.com>
2020-11-21 20:19:16 +0000

    Updated Advanced Rubric to include new tokens

--- client/src/subsites/advanced/AdvancedRubric.jsx
+++ client/src/subsites/advanced/AdvancedRubric.jsx
@@ -13,14 +13,19 @@ const AdvancedRubric = () => {
             <ul>
                 <li>If a capital “C” is input, it means any consonant letter (bcdfghklmnpqrstvxyz).</li>
                 <li>If a capital “V” is input, it means any vowel letter (aeiouy). Even if “i” is pronounced as a consonant, it is treated as a vowel here.</li>
+                <li>If a full stop is input, it means any letter (abcdefghiklmnopqrstuvxyz).</li>
                 <li>If an underscore is input, it means any sequence of zero or more letters.</li>
+                <li>If an exclamation-mark is added after a letter, it makes the letter optional.</li>
             </ul>
             <h4>Examples</h4>
             <ul>
                 <li>“vota” returns the words <Link to="../vo-ta" title="vōta">vōta</Link> and <Link to="../vo-ta-" title="vōtā">vōtā</Link>.</li>
                 <li>“Vota” returns the word <Link to="../io-ta" title="iōta">iōta</Link>.</li>
+                <li>“.ota” returns all four-letter words ending in “ota”.</li>
                 <li>“augustus” returns the words <Link to="../Augustus" title="Augustus">Augustus</Link> and <Link to="../augustus" title="augustus">augustus</Link>. “VVgustus” returns the same. “Augustus” does not return anything: the “a” needs to be lowercase here.</li>
                 <li>“august_” returns all the forms of <Link to="../Augusta" title="Augusta">Augusta</Link>, <Link to="../Augustus" title="Augustus">Augustus</Link>, <Link to="../augustus" title="augustus">augustus</Link>, <Link to="../Augusti-nus" title="Augustīnus">Augustīnus</Link>, etc that are in velut.</li>
+                <li>“h!V_” returns words that either begin with a vowel or begin with “h” plus a vowel.</li>
+                <li>“_Vm!” returns words that either end with a vowel or end with a vowel plus “m”.</li>
                 <li>“_” doesn’t return anything unless scansion is also set (see below).</li>
             </ul>
             <h3>Scansion</h3>
@@ -29,6 +34,7 @@ const AdvancedRubric = () => {
                 <li>“L” means a long syllable — one containing a long vowel or ending with a consonant sound.</li>
                 <li>“S” means a short syllable — any syllable that is not long.</li>
                 <li>“X” is the anceps value, meaning any one syllable, long or short.</li>
+                <li>An exclamation-mark after any of the above makes the syllable optional.</li>
                 <li>An underscore means any sequence of zero or more syllables.</li>
             </ul>
             <p>In words where a vowel is followed by plosive consonant and then “l” or “r”, both consonants belong to the following syllable, so <Link to="../tetradrachmum" title="tetradrachmum">tetradrachmum</Link> begins with two short syllables.</p>
@@ -38,6 +44,7 @@ const AdvancedRubric = () => {
                 <li>“LSS” returns dactyls, such as <Link to="../Graecia" title="Graecia">Graecia</Link> and <Link to="../obdere" title="obdere">obdere</Link>.</li>
                 <li>“XXXX” returns words of four syllables, such as <Link to="../agricola" title="agricola">agricola</Link> and <Link to="../thaumatu-rgus" title="thaumatūrgus">thaumatūrgus</Link>.</li>
                 <li>“LSSLX” returns words that can occupy the fifth and sixth feet of a dactylic hexameter, such as <Link to="../amphithea-trum" title="amphitheātrum">amphitheātrum</Link> and <Link to="../quo-modocumque" title="quōmodocumque">quōmodocumque</Link>.</li>
+                <li>“SSSSSS!” returns words of five or six short syllables, such as <Link to="../liquefacite" title="liquefacite">liquefacite</Link> and <Link to="../Mesopotamia" title="Mesopotamia">Mesopotamia</Link>.</li>
                 <li>“_SSSS_” returns words that contain a run of four short syllables, such as <Link to="../agricola" title="agricola">agricola</Link> (again) and <Link to="../physiologia-" title="physiologiā">physiologiā</Link>.</li>
                 <li>“_” doesn’t return anything unless spelling is also set (see above).</li>
             </ul>
