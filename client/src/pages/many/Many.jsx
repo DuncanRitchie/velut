f958d19fa680a296a1c25ab1012bbf2501597eff
DuncanRitchie <duncanritchie@btinternet.com>
2021-04-16 22:55:59 +0100

    Removed an unneeded import

--- client/src/pages/many/Many.jsx
+++ client/src/pages/many/Many.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
 import Header from '../../components/header/Header'
 import LatinLink from '../../components/latinlink/LatinLink'
 import axios from "../../axios/axios"
