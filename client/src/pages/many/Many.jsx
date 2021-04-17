802560eaa908d45028ca9695a7f6f68fcee443fd
DuncanRitchie <duncanritchie@btinternet.com>
2021-04-17 21:41:44 +0100

    <Many> handles subsequent searches correctly

--- client/src/pages/many/Many.jsx
+++ client/src/pages/many/Many.jsx
@@ -41,25 +41,41 @@ class Many extends Component {
     fetchWords = () => {
         console.log("Fetching words...")
         const searchedWords = this.splitInputIntoWords();
-        //// searchedWords may contain duplicates.
-        //// pendingWords and distinctWords should initially be the same set of distinct words that were entered.
-        //// pendingWords’ being a set means we can delete words from it when they are no longer pending.
-        //// distinctWords needs to be an array so it can be mapped over in the render method.
+        //// `searchedWords` may contain duplicates.
+        //// `pendingWords` and distinctWords should initially be the same set of distinct words that were entered.
+        //// Because `pendingWords`’ is a set, we can delete words from it when they are no longer pending.
+        //// `distinctWords` needs to be an array so it can be mapped over in the render method.
         const pendingWords = new Set(searchedWords)
         const distinctWords = [...pendingWords]
         this.setState({
-            countWordsLoading: distinctWords.length,
             distinctWords,
             pendingWords,
             foundWords: [],
             missingWords: [],
         }, ()=>{
             distinctWords.forEach(word => {
-                if (!this.state.allWords.has(word)) {
+                //// If words from previous searches are in `allWords`, we don’t need to re-fetch them,
+                //// but they need to be re-added to `foundWords` and `missingWords`.
+                if (this.state.allWords.has(word)) {
+                    let {pendingWords, foundWords, missingWords} = this.state
+                    pendingWords.delete(word)
+                    //// `word` will be in `allWords` as `undefined` if it’s not in velut
+                    if (this.state.allWords.get(word)) {
+                        foundWords.push(word)
+                    }
+                    else {
+                        missingWords.push(word)
+                    }
+                    this.setState({pendingWords, foundWords, missingWords})
+                }
+                //// New words need to be fetched from the back-end.
+                else {
                     axios.getOneWordSelectOnlyWord(word)
                         .then(response => {
                             const foundWord = response.data.Word
                             let {allWords, pendingWords, foundWords, missingWords} = this.state
+                            //// If the word is in velut, the value of `foundWord` is that of the Word field, ie simply the macronized word.
+                            //// If the word is not in velut, it will still be added to `allWords`, but its value will be `undefined`.
                             allWords.set(word, foundWord)
                             pendingWords?.delete(word)
                             if (foundWord) {
