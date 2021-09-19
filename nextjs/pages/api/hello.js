e0eb0fda5effabc96f99cf00233165e81056d70a
DuncanRitchie <duncanritchie@btinternet.com>
2021-09-19 22:29:12 +0100

    Removed two console.logs

--- nextjs/pages/api/hello.js
+++ nextjs/pages/api/hello.js
@@ -1,8 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 const hello = async (req, res) => {
-  console.log({req})
-  console.log({res});
   res.status(200).json({ name: 'John Doe' })
 }
 
