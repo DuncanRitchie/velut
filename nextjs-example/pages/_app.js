c1053527aa24c217b4beea959e38bcf949e1fcef
DuncanRitchie <duncanritchie@btinternet.com>
2021-09-25 17:21:13 +0100

    Brought in fonts, images, & _app.js

--- nextjs-example/pages/_app.js
+++ nextjs-example/pages/_app.js
@@ -1,36 +1,11 @@
-import '../css/style.css'
-import '../css/form.css'
-import Head from 'next/head'
-import Link from 'next/link'
+import styles from '../css/globals.css'
+import Footer from '../components/footer/Footer'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Word Care App</title>
-      </Head>
-
-      <div className="top-bar">
-        <div className="nav">
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          <Link href="/new">
-            <a>Add Word</a>
-          </Link>
-        </div>
-
-        <img
-          id="title"
-          src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Pet_logo_with_flowers.png"
-          alt="pet care logo"
-        ></img>
-      </div>
-      <div className="grid wrapper">
-        <Component {...pageProps} />
-      </div>
-    </>
-  )
+function App({ Component, pageProps }) {
+  return (<>
+    <Component {...pageProps} />
+    <Footer />
+  </>)
 }
 
-export default MyApp
+export default App
