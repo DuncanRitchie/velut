25ad4be5f1f88163d8251a7b5a85ae6a00add488
DuncanRitchie <duncanritchie@btinternet.com>
2021-09-25 20:45:40 +0100

    Refactored superscriptLemmaTag

--- nextjs-example/components/lemma/superscriptLemmaTag.jsx
+++ nextjs-example/components/lemma/superscriptLemmaTag.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
+
+//// If there are multiple lemmata in the database with the same headword,
+//// I add extra info in square brackets to disambiguate.
+//// Eg "amīcus[n]" & "amīcus[adj]" because “amīcus” can be noun or adjective.
+//// This function converts this text into JSX, with the bracketed bit
+//// (if present) rendered in round brackets with a CSS class.
+//// Eg "amīcus[n]" -> <>amīcus <span className="lemma-tag">(n)</span>
+//// Eg "fūlmārus" -> <>fūlmārus</>
 const superscriptLemmaTag = (lemma) => {
-    if (lemma) {
-        let array = lemma.split(/[[\]]/g)
-        if (array[1]) {
-            return <>{array[0]}&nbsp;<span className="lemma-tag">({array[1]})</span></>
-        }
-        else {
-            return <>{lemma}</>
-        }
-    }
-    else {
+    if (!lemma) {
         return null;
     }
+
+    //// Split the string on [ and ]
+    let array = lemma.split(/[[\]]/g)
+    //// Any text in square brackets becomes `array[1]`
+    if (!array[1]) {
+        return <>{lemma}</>
+    }
+
+    return <>{array[0]}&nbsp;<span className="lemma-tag">({array[1]})</span></>
 }
 export default superscriptLemmaTag
