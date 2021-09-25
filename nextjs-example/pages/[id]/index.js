7e565abba0f9386ac9341ae3708dc18025aaaa6c
DuncanRitchie <duncanritchie@btinternet.com>
2021-09-25 21:30:08 +0100

    URLs other than / and /about tell you whether it’s a word in velut (assuming no diacritics)

--- nextjs-example/pages/[id]/index.js
+++ nextjs-example/pages/[id]/index.js
@@ -5,71 +5,48 @@ import dbConnect from '../../lib/dbConnect'
 import Word from '../../models/Word'
 
 /* Allows you to view word card info and delete word card*/
-const WordPage = ({ word }) => {
+const WordPage = ({ word, search }) => {
   const router = useRouter()
   const [message, setMessage] = useState('')
-  const handleDelete = async () => {
-    const wordID = router.query.id
+  // const handleDelete = async () => {
+  //   const wordID = router.query.id
 
-    try {
-      await fetch(`/api/words/${wordID}`, {
-        method: 'Delete',
-      })
-      router.push('/')
-    } catch (error) {
-      setMessage('Failed to delete the word.')
+  //   try {
+  //     await fetch(`/api/words/${wordID}`, {
+  //       method: 'Delete',
+  //     })
+  //     router.push('/')
+  //   } catch (error) {
+  //     setMessage('Failed to delete the word.')
+  //   }
+  // }
+
+    if (word) {
+        return (
+            <div key={word._id}>
+                <div className="card">
+                    <img src={word.image_url} />
+                    <h2 className="word-name">{word.Word}</h2>
+                    <p>It’s in the dictionary!</p>
+                </div>
+            </div>
+        )
     }
-  }
-
-  return (
-    <div key={word._id}>
-      <div className="card">
-        <img src={word.image_url} />
-        <h5 className="word-name">{word.name}</h5>
-        <div className="main-content">
-          <p className="word-name">{word.name}</p>
-          <p className="owner">Owner: {word.owner_name}</p>
-
-          {/* Extra Word Info: Likes and Dislikes */}
-          <div className="likes info">
-            <p className="label">Likes</p>
-            <ul>
-              {word.likes.map((data, index) => (
-                <li key={index}>{data} </li>
-              ))}
-            </ul>
-          </div>
-          <div className="dislikes info">
-            <p className="label">Dislikes</p>
-            <ul>
-              {word.dislikes.map((data, index) => (
-                <li key={index}>{data} </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="btn-container">
-            <Link href="/[id]/edit" as={`/${word._id}/edit`}>
-              <button className="btn edit">Edit</button>
-            </Link>
-            <button className="btn delete" onClick={handleDelete}>
-              Delete
-            </button>
-          </div>
-        </div>
-      </div>
-      {message && <p>{message}</p>}
-    </div>
-  )
+    else {
+        return (
+            <>Word “{search}” not found!</>
+        )
+    }
 }
 
 export async function getServerSideProps({ params }) {
   await dbConnect()
 
-  const word = await Word.findById(params.id).lean()
-  word._id = word._id.toString()
+  const word = await Word.findOne({ "Word": params.id }).select({ "_id": 0 })
+  const wordAsObject = word?.toObject() ?? null
+  console.log({wordAsObject})
 
-  return { props: { word } }
+  return { props: { word: wordAsObject, search: params.id } }
 }
 
 export default WordPage
