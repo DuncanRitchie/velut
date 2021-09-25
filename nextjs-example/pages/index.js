3557acc5fe5b2427ff6392d7e0c6c988f2618e92
DuncanRitchie <duncanritchie@btinternet.com>
2021-09-25 21:54:33 +0100

    Home page, albeit without Search

--- nextjs-example/pages/index.js
+++ nextjs-example/pages/index.js
@@ -1,16 +1,47 @@
-import Link from 'next/link'
-import dbConnect from '../lib/dbConnect'
-import Word from '../models/Word'
+import React from 'react'
+import Head from 'next/head'
+import Header from "../components/header/Header"
+// import Search from '../components/search/Search'
+import styles from '../css/Home.module.css'
 
-const Index = ({ words }) => (
-  <>
-    {/* Create a card for each word */}
-    {words.map((word) => (
-      <div key={word._id}>
-        <div className="card">
-          <p>{word.Word}</p>
-          {/* <img src={word.image_url} />
-          <h5 className="word-name">{word.name}</h5>
+const Home = () => {
+    return (<>
+        <Head>
+            <title>Duncan Ritchie’s velut — a Latin rhyming dictionary</title>
+        </Head>
+        <div className={styles.home + " fulmar-background"}>
+            <Header />
+            <h2 className="home-rubric">
+                <span>Latin rhymes &amp;&nbsp;more!</span>
+            </h2>
+            {/* <Search prefix="" searchbarTitle="Type a Latin word" /> */}
+            <p className="home-rubric">
+                <span>Enter a Latin word in the searchbar above,</span><br/>
+                <span>or see the navigation bar below for other options.</span>
+            </p>
+        </div>
+    </>)
+}
+
+export default Home
+
+
+
+
+
+// import Link from 'next/link'
+// import dbConnect from '../lib/dbConnect'
+// import Word from '../models/Word'
+
+// const Index = ({ words }) => (
+//   <>
+//     {/* Create a card for each word */}
+//     {words.map((word) => (
+//       <div key={word._id}>
+//         <div className="card">
+//           <p>{word.Word}</p>
+//          {<img src={word.image_url} />
+          /* <h5 className="word-name">{word.name}</h5>
           <div className="main-content">
             <p className="word-name">{word.name}</p>
             <p className="owner">Owner: {word.owner_name}</p>
@@ -40,26 +71,26 @@ const Index = ({ words }) => (
                 <button className="btn view">View</button>
               </Link>
             </div>
-          </div> */}
-        </div>
+          </div> */
+        /* </div>
       </div>
     ))}
   </>
-)
+) */
 
 /* Retrieves word(s) data from mongodb database */
-export async function getServerSideProps() {
-  await dbConnect()
+// export async function getServerSideProps() {
+//   await dbConnect()
 
   /* find all the data in our database */
-  const result = await Word.find({"Word": "fūlmārus"})
-  const words = result.map((doc) => {
-    const word = doc.toObject()
-    word._id = word._id.toString()
-    return word
-  })
+//   const result = await Word.find({"Word": "fūlmārus"})
+//   const words = result.map((doc) => {
+//     const word = doc.toObject()
+//     word._id = word._id.toString()
+//     return word
+//   })
 
-  return { props: { words: words } }
-}
+//   return { props: { words: words } }
+// }
 
-export default Index
+// export default Index
