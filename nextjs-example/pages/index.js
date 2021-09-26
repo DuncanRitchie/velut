67af4e10ba0cc300006cbd1ffbadf2b209980464
DuncanRitchie <duncanritchie@btinternet.com>
2021-09-26 14:17:58 +0100

    Corrected CSS classes on Home

--- nextjs-example/pages/index.js
+++ nextjs-example/pages/index.js
@@ -11,11 +11,11 @@ const Home = () => {
         </Head>
         <div className={styles.home + " fulmar-background"}>
             <Header />
-            <h2 className="home-rubric">
+            <h2 className={styles.homeRubric}>
                 <span>Latin rhymes &amp;&nbsp;more!</span>
             </h2>
             <Search prefix="" searchbarTitle="Type a Latin word" autofocus={true} />
-            <p className="home-rubric">
+            <p className={styles.homeRubric}>
                 <span>Enter a Latin word in the searchbar above,</span><br/>
                 <span>or see the navigation bar below for other options.</span>
             </p>
