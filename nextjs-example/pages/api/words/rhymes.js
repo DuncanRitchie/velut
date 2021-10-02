d43b698333ee6fba2d02363320d0012cb225fc77
DuncanRitchie <duncanritchie@btinternet.com>
2021-10-02 18:26:17 +0100

    Rhymes are sorted according to their type

--- nextjs-example/pages/api/words/rhymes.js
+++ nextjs-example/pages/api/words/rhymes.js
@@ -12,7 +12,7 @@ export default async function getRhymes(wordObject, type) {
         // Let’s find the rhymes.
         // We will be using the following three values if route-specific values are not found in routes.json.
         let searchField = "PerfectRhyme" // This is the MongoDB fieldname.
-        let axiosFuncName = "getWordsClass" // This is one of "getWordsAlph", "getWordsClass", "getWordsEccles" depending on the sort wanted.
+        let sortFields = "Sort" // This is one of "Sort", "EcclesSort", "NoMacraLowerCase NoMacra Word" depending on the sort wanted.
         let searchFieldFull = "Perfect rhymes (classical)" // This will be rendered onscreen as a heading.
         // The route determines which type of rhyme will be wanted.
         // routes is routes.json, which matches routes to searchField, axiosFuncName, and searchFieldFull.
@@ -20,13 +20,13 @@ export default async function getRhymes(wordObject, type) {
         const routeObject = routes.find(route=>{return ("/"+type === route.route)})
         if (routeObject) {
             searchField = routeObject.searchField
-            axiosFuncName = routeObject.axiosFuncName
+            sortFields = routeObject.sortFields
             searchFieldFull = routeObject.searchFieldFull
         }
         let query = {[searchField]: wordObject[searchField]}
 
         const foundWords = await Word.find(query)
-            .sort("Sort")
+            .sort(sortFields)
             .select({"Word": 1, "_id": 0})
             .exec()
         console.log({foundWords})
@@ -44,60 +44,4 @@ export default async function getRhymes(wordObject, type) {
         console.log(error)
         return { success: false, error: error.toString() }
     }
-}
-
-
-
-
-const fetchRhymes = (wordObject, type) => {
-    // Let’s find the rhymes.
-    // We will be using the following three values if route-specific values are not found in routes.json.
-    let searchField = "PerfectRhyme" // This is the MongoDB fieldname.
-    let axiosFuncName = "getWordsClass" // This is one of "getWordsAlph", "getWordsClass", "getWordsEccles" depending on the sort wanted.
-    let searchFieldFull = "Perfect rhymes (classical)" // This will be rendered onscreen as a heading.
-    // The route determines which type of rhyme will be wanted.
-    // routes is routes.json, which matches routes to searchField, axiosFuncName, and searchFieldFull.
-    // Let’s retrieve the values we want.
-    const routeObject = routes.find(route=>{return ("/"+type === route.route)})
-    if (routeObject) {
-        searchField = routeObject.searchField
-        axiosFuncName = routeObject.axiosFuncName
-        searchFieldFull = routeObject.searchFieldFull
-    }
-    let query = {[searchField]: wordObject[searchField]}
-
-    // axios[axiosFuncName](query).then((data)=>{
-    //     let rhymes = data.data.map((wordObject,index)=>{
-    //         return wordObject.Word
-    //     })
-    // })
-
-    return {
-        rhymes: [],
-        searchField,
-    }
-}
-const findWordsClassical = function(req, res) {
-	console.log(req);
-	Word.find(req.query)
-		.sort("Sort")
-		.select({"Word": 1, "_id": 0})
-		.then(words => {res.json(words)})
-		.catch(err => res.status(422).json(err))
-}
-
-const findWordsEcclesiastical = function(req, res) {
-	Word.find(req.query)
-		.sort("EcclesSort")
-		.select({"Word": 1, "_id": 0})
-		.then(words => {res.json(words)})
-		.catch(err => res.status(422).json(err))
-}
-
-const findWordsAlphabetical = function(req, res) {
-	Word.find(req.query)
-		.sort("NoMacraLowerCase NoMacra Word")
-		.select({"Word": 1, "_id": 0})
-		.then(words => res.json(words))
-		.catch(err => res.status(422).json(err))
 }
