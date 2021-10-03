2c08cc3795b04080c93088ce1f8618d45ac738c6
DuncanRitchie <duncanritchie@btinternet.com>
2021-10-03 14:07:28 +0100

    English results are sorted correctly

--- nextjs-example/pages/api/lemmata/english.js
+++ nextjs-example/pages/api/lemmata/english.js
@@ -16,11 +16,60 @@ export default async function english(input) {
             "$options": "i"
         }
     }
-    const sort = (a,b)=>{
-        return (Math.random()-0.5)
-    }
+    const sort = getSortFunction(escapedInput)
 
     return getLemmata(query, sort)
+}
+
+//// Returns a sort function that will sort an array of lemmata to
+//// prioritise the closest matches for `queryWord` in the Meanings field.
+//// The returned function can then be passed into the `array.sort` method.
+const getSortFunction = (queryWord) => {
+
+    //// Local helper function to use within the sort function.
+    const lemmaHasTheQueriedMeaning = (lemma) => {
+        const meaningsArray = lemma.Meanings.split("; ")
+        for (let i = 0; i < meaningsArray.length; i++) {
+            if (meaningsArray[i].toLowerCase() === queryWord.toLowerCase()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    return (a,b)=>{
+        // A lemma with the query as an entire meaning gets prioritised.
+        // E.g. homō “man; person…” precedes adulēscentulus “young man” if the query is “man”.
+        const aHasTheQueriedMeaning = lemmaHasTheQueriedMeaning(a);
+        const bHasTheQueriedMeaning = lemmaHasTheQueriedMeaning(b);
+        if (aHasTheQueriedMeaning && !bHasTheQueriedMeaning) {
+            return -1
+        }
+        else if (!aHasTheQueriedMeaning && bHasTheQueriedMeaning) {
+            return 1
+        }
+
+        // A lemma with Meanings containing query as an entire word gets prioritised.
+        // E.g. Oriēns “the East” precedes belua “beast” if query is “east”.
+        const regex = RegExp("[\\b\\s\\W\\A ]" + queryWord + "[\\b\\s\\W\\Z ]", "i")
+        const aContainsWholeWord = regex.test(" " + a.Meanings + " ");
+        const bContainsWholeWord = regex.test(" " + b.Meanings + " ");
+        if (aContainsWholeWord && !bContainsWholeWord) {
+            return -1
+        }
+        else if (!aContainsWholeWord && bContainsWholeWord) {
+            return 1
+        }
+
+        // A lemma with shorter Meanings gets prioritised.
+        // E.g. Thalīa “muse of comedy” (14 chars) precedes Ūrania “muse of astronomy” (17 chars) if query is “muse”.
+        else if (a.Meanings.length === b.Meanings.length) {
+            return a.Meanings > b.Meanings
+        }
+        else {
+            return a.Meanings.length - b.Meanings.length
+        }
+    }
 }
 
 //// Old controller from CRA:
