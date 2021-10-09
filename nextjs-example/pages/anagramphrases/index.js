88dafe3e0f8edc140f5373c8cf3fa5cc87b6114a
DuncanRitchie <duncanritchie@btinternet.com>
2021-10-09 20:42:28 +0100

    Anagram Phrases homepage

---
+++ nextjs-example/pages/anagramphrases/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Head from 'next/head'
+import Header from '../../components/header/Header'
+import Search from '../../components/search/Search'
+import styles from '../../css/Subsites.module.css'
+
+const AnagramsHome = () => {
+    return (<>
+        <Head>
+            <title>
+                Anagram phrases on velut — a Latin rhyming dictionary
+            </title>
+        </Head>
+        <div className={styles.subsiteHome+" anagram-phrases fulmar-background"}>
+            <Header textBeforeTitle="Anagram phrases"/>
+            <p className={styles.subsiteHomeRubric}>
+                This will help you find Latin phrases that are anagrams!
+            </p>
+            <p className={styles.subsiteHomeRubric}>
+                Caution — searches may take some minutes. Searches longer than ten characters may fail completely.
+            </p>
+            <Search
+              prefix="anagramphrases/"
+              searchbarTitle="Type something to find anagrams of"
+              lang="zxx"
+              hideDropdown={true}
+              type="/anagramphrases"
+            />
+        </div>
+    </>)
+}
+
+export default AnagramsHome
