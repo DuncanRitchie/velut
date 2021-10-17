DuncanRitchie/velut

commit bb5694a8970b6e0eb5d0cfcbd9353d247771b378

Removed `fromUrl` from Advanced Search state because it’s not used

nextjs-example/components/advancedComponents/AdvancedSearch.jsx
@@ -17,7 +17,6 @@ class AdvancedSearch extends Component {
             },
             elision: false,
             sort: "alphabetical",
-            fromUrl: true,
         }
     }
 
@@ -49,10 +48,11 @@ class AdvancedSearch extends Component {
         } catch {
             sanitisedInput = input;
         }
-        let newState = {fromUrl: false};
-        newState[name] = {
-            unsanitised: input,
-            sanitised: sanitisedInput
+        const newState = {
+            name: {
+                unsanitised: input,
+                sanitised: sanitisedInput,
+            }
         };
         this.setState(newState);
     }
@@ -110,7 +110,6 @@ class AdvancedSearch extends Component {
     componentDidUpdate(prevProps) {
         const locationChanged = this.props.location !== prevProps.location
         if (locationChanged) {
-            this.setState({fromUrl: true})
             this.setStateFromUrl();
         }
     }

commit 4d09bd17a83eab7b0d9401d18edc9e1f95f45c73

Fixed bug introduced in bb5694a8

nextjs-example/components/advancedComponents/AdvancedSearch.jsx
@@ -48,12 +48,11 @@ class AdvancedSearch extends Component {
         } catch {
             sanitisedInput = input;
         }
-        const newState = {
-            name: {
-                unsanitised: input,
-                sanitised: sanitisedInput,
-            }
-        };
+        let newState = {}
+        newState[name] = {
+            unsanitised: input,
+            sanitised: sanitisedInput,
+        }
         this.setState(newState);
     }
 