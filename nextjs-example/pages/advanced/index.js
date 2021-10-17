12791890dfd295baf0dea6e1a1e1e0b7388fdd7e
DuncanRitchie <duncanritchie@btinternet.com>
2021-10-17 22:57:14 +0100

    Advanced displays a link for each result

--- nextjs-example/pages/advanced/index.js
+++ nextjs-example/pages/advanced/index.js
@@ -1,26 +1,80 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { Fragment } from 'react'
 import Header from '../../components/header/Header'
 import AdvancedRubricToggler from '../../components/advancedComponents/AdvancedRubricToggler'
 import AdvancedSearch from '../../components/advancedComponents/AdvancedSearch'
 import findAdvanced from '../api/words/advanced'
+import { macraToHyphens } from '../api/diacritics'
 import styles from '../../css/Subsites.module.css'
 
-const AdvancedHome = ({ query }) => {
-    return (<>
-        <Head>
-            <title>
-                Advanced Search on velut — a Latin rhyming dictionary
-            </title>
-        </Head>
-        <div className={styles.subsiteHome + " advanced fulmar-background"}>
-            <Header textBeforeTitle="Advanced Search"/>
-            <AdvancedRubricToggler/>
-            <AdvancedSearch autofocus={true} query={query}/>
-        </div>
-    </>)
+const Advanced = ({ query, isHomepage, words, error }) => {
+    if (isHomepage) {
+        return (<>
+            <Head>
+                <title>
+                    Advanced Search on velut — a Latin rhyming dictionary
+                </title>
+            </Head>
+            <div className={styles.subsiteHome + " advanced fulmar-background"}>
+                <Header textBeforeTitle="Advanced Search"/>
+                <AdvancedRubricToggler/>
+                <AdvancedSearch autofocus={true} query={query}/>
+            </div>
+        </>)
+    }
+    else {
+        let mappedWords = []
+        if (words) {
+            // Render a Link and a space for every word.
+            mappedWords = words.map((word,index)=>{
+                return (
+                    <Fragment key={index}><Link href={`../${macraToHyphens(word)}`}>
+                        <a lang="la"title={word}>
+                            {word}
+                        </a>
+                    </Link> </Fragment>
+                )
+            })
+        }
+        let result = null
+        if (error) {
+            result = (<p>There was an error! Please try a different search. {error}</p>)
+        }
+        else if (mappedWords.length) {
+            result = (
+                <div>
+                    <p>Here {mappedWords.length === 1 ? "is the 1 Latin word that fits" : `are the ${mappedWords.length} Latin words that fit`} the search.</p>
+                    <p>{mappedWords}</p>
+                </div>
+            )
+        }
+        else {
+            result = (
+                <p>No results found! Please try a different search.</p>
+            )
+        }
+        return (<>
+            <Head>
+                <title>
+                    Advanced Search on velut — a Latin rhyming dictionary
+                </title>
+            </Head>
+            <div className="advanced fulmar-background">
+                <Header textBeforeTitle="Advanced Search" />
+                <div>
+                    <AdvancedRubricToggler/>
+                    <AdvancedSearch autofocus={false} query={query}/>
+                    <div className="subsite-result">
+                        {result}
+                    </div>
+                </div>
+            </div>
+        </>)
+    }
 }
 
-export default AdvancedHome
+export default Advanced
 
 export async function getServerSideProps(props) {
     console.log(props)
@@ -33,14 +87,14 @@ export async function getServerSideProps(props) {
         const results = await findAdvanced(query)
         console.log(results)
         return { props: {
-            showResults: true,
+            isHomepage: false,
             query,
             ...results,
         }}
     }
     else {
         return { props: {
-            showResults: false,
+            isHomepage: true,
             query,
         }}
     }
