5ea32ba0dededcb2010d082bed62acbd72f974b0
DuncanRitchie <duncanritchie@btinternet.com>
2021-10-24 22:46:48 +0100

    Default Head component

---
+++ nextjs-example/components/defaultHead/DefaultHead.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+function DefaultHead() {
+    return (
+        <Head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link rel="shortcut icon" href="https://www.duncanritchie.co.uk/favicon.ico" />
+            <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
+            <meta name="theme-color" content="#000000" />
+            {/* Page title & description should get overridden on individual pages; below are fallbacks. */}
+            <title>velut — a Latin rhyming dictionary</title>
+            <meta name="Description" content="velut — a Latin dictionary with lists of rhymes, anagrams, homographs, consonyms, subwords, inflected forms, cognates, and links to other online resources." />
+        </Head>
+    )
+}
+
+export default DefaultHead
--- nextjs-example/pages/_app.js
+++ nextjs-example/pages/_app.js
@@ -1,8 +1,10 @@
+import DefaultHead from '../components/defaultHead/DefaultHead'
 import styles from '../css/globals.css'
 import Footer from '../components/footer/Footer'
 
 function App({ Component, pageProps }) {
   return (<>
+    <DefaultHead />
     <Component {...pageProps} />
     <Footer />
   </>)
