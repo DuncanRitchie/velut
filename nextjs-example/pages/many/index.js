0b0c05dfac89ad1bbbdc3d7729673090af06c8ad
DuncanRitchie <duncanritchie@btinternet.com>
2021-10-24 19:23:53 +0100

    Deleted unused code from Many

--- nextjs-example/pages/many/index.js
+++ nextjs-example/pages/many/index.js
@@ -1,23 +1,5 @@
-// import React from 'react';
-// import Link from 'next/link'
-// import Head from 'next/head'
-// import Header from '../../components/header/Header'
-// import Search from '../../components/search/Search'
-// import styles from '../../css/Subsites.module.css'
-
-// const Many = ({}) => {
-//     return (<>
-//         <Head>
-//             <title>
-//                 Subwords on velut — a Latin rhyming dictionary
-//             </title>
-//         </Head>
-//     </>)
-// }
-
 import React, {Component, Fragment} from 'react'
 import Head from 'next/head'
-//import {withRouter} from 'react-router-dom'
 import Redirect from '../../components/redirect/Redirect'
 import Header from '../../components/header/Header'
 import LatinLink from '../../components/latinlink/LatinLink'
@@ -26,21 +8,14 @@ import subsitesStyles from '../../css/Subsites.module.css'
 import manyStyles from '../../css/Many.module.css'
 import searchStyles from '../../components/search/Search.module.css'
 
-// <Many/> is a JSX element rendered at /many/:input
+// <Many/> is a JSX element rendered at /many/?search=:input
 
 class Many extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            fromUrl: true,
             input: "",
             searchedWords: [],
-            allWords: new Map(),
-            distinctWords: new Set(),
-            pendingWords: new Set(),
-            countWordsLoading: 0,
-            foundWords: new Set(),
-            missingWords: new Set(),
             navigating: false,
             newUrl: "",
         }
@@ -60,13 +35,6 @@ class Many extends Component {
         this.setState({searchedWords});
         return searchedWords;
     }
-
-    // setUrlFromInput = (searchedWordsArray) => {
-    //     const searchedWordsAsString = searchedWordsArray.join(" ");
-    //     const urlParams = new URLSearchParams([["search", searchedWordsAsString]]);
-    //     const newUrl = `../../many/?${urlParams}`;
-    //     this.s.history.push(newUrl);
-    // }
 
     setTextAreaFromUrl = () => {
         const urlParams = new URLSearchParams(this.props.location.search);
@@ -90,78 +58,11 @@ class Many extends Component {
         })
     }
 
-    // fetchWords = (urlShouldBeChanged = true) => {
-    //     const searchedWords = this.splitInputIntoWords();
-    //     //// `searchedWords` may contain duplicates.
-    //     //// `pendingWords` and distinctWords should initially be the same set of distinct words that were entered.
-    //     //// Because `pendingWords`’ is a set, we can delete words from it when they are no longer pending.
-    //     //// `distinctWords` needs to be an array so it can be mapped over in the render method.
-    //     const pendingWords = new Set(searchedWords)
-    //     const distinctWords = [...pendingWords]
-    //     if (urlShouldBeChanged) {
-    //         //this.setUrlFromInput(searchedWords);
-    //     }
-    //     this.setState({
-    //         distinctWords,
-    //         pendingWords,
-    //         foundWords: new Set(),
-    //         missingWords: new Set(),
-    //     }, ()=>{
-    //         distinctWords.forEach(word => {
-    //             //// If words from previous searches are in `allWords`, we don’t need to re-fetch them,
-    //             //// but they need to be re-added to `foundWords` and `missingWords`.
-    //             if (this.state.allWords.has(word)) {
-    //                 let {pendingWords, foundWords, missingWords} = this.state
-    //                 pendingWords.delete(word)
-    //                 //// `word` will be in `allWords` as `undefined` if it’s not in velut
-    //                 if (this.state.allWords.get(word)) {
-    //                     foundWords.add(word)
-    //                 }
-    //                 else {
-    //                     missingWords.add(word)
-    //                 }
-    //                 this.setState({pendingWords, foundWords, missingWords})
-    //             }
-    //             //// New words need to be fetched from the back-end.
-    //             else {
-    //                 // axios.getOneWordSelectOnlyWord(word)
-    //                 //     .then(response => {
-    //                 //         const foundWord = response.data.Word
-    //                 //         let {allWords, pendingWords, foundWords, missingWords} = this.state
-    //                 //         //// If the word is in velut, the value of `foundWord` is that of the Word field, ie simply the macronized word.
-    //                 //         //// If the word is not in velut, it will still be added to `allWords`, but its value will be `undefined`.
-    //                 //         allWords.set(word, foundWord)
-    //                 //         pendingWords?.delete(word)
-    //                 //         if (foundWord) {
-    //                 //             foundWords.add(word)
-    //                 //         } else {
-    //                 //             missingWords.add(word)
-    //                 //         }
-    //                 //         this.setState({allWords, pendingWords, foundWords, missingWords})
-    //                 //     });
-    //                 }
-    //             }
-    //         )
-    //     })
-    // }
-
     /* My velut-dictionary-links site generates links to several Latin websites, based on the "words" parameter in the query-string. */
     getHrefForDictionaryLinks(wordsToGetLinksFor) {
         const dictionaryLinksQuery = new URLSearchParams([["words", wordsToGetLinksFor.join(" ")]]);
         return `https://www.duncanritchie.co.uk/velut-dictionary-links/?${dictionaryLinksQuery}`;
     }
-
-    // componentDidMount() {
-    //     this.setTextAreaFromUrl();
-    //     this.fetchWords(false);
-    // }
-
-    // componentDidUpdate(prevProps) {
-    //     const searchChanged = this.props.location.search !== prevProps.location.search
-    //     if (searchChanged) {
-    //         this.setTextAreaFromUrl();
-    //     }
-    // }
 
     render() {
         console.log(this.props)
@@ -197,8 +98,6 @@ class Many extends Component {
             const foundWordsCount    = foundWords.length
             const missingWordsCount  = missingWords.length
             const allWordsCount      = this.props.distinctWords.length
-            // const pendingWordsCount  = this.props.pendingWords.size
-            // const proportionComplete = 1 - pendingWordsCount / allWordsCount
 
             result = (
                 <div>
