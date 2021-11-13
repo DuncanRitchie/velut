6d6c3c84a2ec20bac6be982bd2aeb260541e9fdb
DuncanRitchie <duncanritchie@btinternet.com>
2021-11-13 16:44:40 +0000

    404 error page

---
+++ pages/404.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Head from 'next/head'
+import Header from "../components/header/Header"
+import Search from '../components/search/Search'
+import styles from '../css/Home.module.css'
+
+const FourOFour = ({type = "/"}) => {
+    return (<>
+        <Head>
+            <title>Page not found on velut — a Latin rhyming dictionary</title>
+        </Head>
+        <div className={styles.home + " fulmar-background"}>
+            <Header textBeforeTitle="Page not found" />
+            <Search prefix="" searchbarTitle="Type a Latin word" autofocus={true} type={type} />
+            <p className={styles.homeRubric}>
+                <span>Please try searching for something else!</span>
+            </p>
+        </div>
+    </>)
+}
+
+export default FourOFour
