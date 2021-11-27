8967d7449cb6eeb9b94c3989e6947e6c393c6d9a
DuncanRitchie <duncanritchie@btinternet.com>
2021-11-27 13:22:07 +0000

    <html> has `lang` attribute because of _document.js

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+//// This file exists to give <html> a `lang` attribute.
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
