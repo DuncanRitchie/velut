f46183521c4a2c2c0b596d393d521a807cf4c649
DuncanRitchie <duncanritchie@btinternet.com>
2022-02-26 16:30:22 +0000

    AdvancedRubric is shown via <details>/<summary>

--- components/advancedComponents/AdvancedRubricToggler.jsx
+++ components/advancedComponents/AdvancedRubricToggler.jsx
@@ -1,33 +1,13 @@
-import {Component} from 'react';
 import AdvancedRubric from './AdvancedRubric';
 import styles from './AdvancedRubricToggler.module.css';
 
-class AdvancedRubricToggler extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showRubric: false,
-        }
-    }
-
-    handleToggler = () => {
-        const showRubric = this.state.showRubric;
-        this.setState({showRubric: !showRubric});
-    }
-
-    render() {
-        return (
-            <>
-                <button
-                    className={styles.advancedRubricTogglerButton}
-                    onClick={this.handleToggler}
-                >
-                    {this.state.showRubric ? "Hide" : "Show"} how to use the Advanced Search
-                </button>
-                {this.state.showRubric ? <AdvancedRubric/> : null}
-            </>
-        )
-    }
+function AdvancedRubricToggler() {
+    return (
+        <details className={styles.advancedRubricToggler}>
+            <summary>How to use the Advanced Search</summary>
+            <AdvancedRubric/>
+        </details>
+    )
 }
 
 export default AdvancedRubricToggler;
