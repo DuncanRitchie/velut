ca81b73f873c66612ae5a110a39c28eeaf04e7cd
DuncanRitchie <duncanritchie@btinternet.com>
2022-05-15 20:28:05 +0100

    500 page, because Anagram Phrases keeps throwing in production

---
+++ pages/500.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Header from "../components/header/Header"
+import Search from '../components/search/Search'
+import styles from '../css/Home.module.css'
+
+const FiveOFive = ({type = "/"}) => {
+    return (<>
+        <Head>
+            <title>Internal server error on velut — a Latin rhyming dictionary</title>
+        </Head>
+        <div className={styles.home + " fulmar-background"}>
+            <Header textBeforeTitle="Internal server error" />
+            <Search prefix="" searchbarTitle="Type a Latin word" autofocus={true} type={type} />
+            <p className={styles.homeRubric}>
+                <span>Please try another page or come back later.</span>
+            </p>
+        </div>
+    </>)
+}
+
+export default FiveOFive
