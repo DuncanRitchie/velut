0993d685d5eb777c1765801ffcce077c11c12602
DuncanRitchie <duncanritchie@btinternet.com>
2022-05-15 16:05:08 +0100

    Removed some console.logs

--- pages/api/word/isInDictionary/index.js
+++ pages/api/word/isInDictionary/index.js
@@ -1,9 +1,7 @@
 import { findOneWordSelectOnlyWord } from "../../../../lib/words/word";
 
 export default async function findOneWord(req, res) {
-    console.log(req.query)
     const foundWord = await findOneWordSelectOnlyWord(req.query.word);
-    console.log({foundWord})
     res.send(foundWord);
     // if (foundWord.success) {
     //     const subwordsAsWords = foundWord.subwords.map((object)=>{
