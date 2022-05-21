48c7c19e359a33dc67f1ddeea5f283ce7b38a236
DuncanRitchie <duncanritchie@btinternet.com>
2022-05-21 14:51:16 +0100

    Revert "Trying to write regexes for the subwords route"
    This reverts commit 1db1da556b19bcae7c1d95525b1416db001bf353.

--- controllers/words-controller.js
+++ controllers/words-controller.js
@@ -1,7 +1,6 @@
 const Word = require('../models/word-model')
 const findSubwordsFromMongo = require('./findSubwordsFromMongo')
 const findAnagrams = require('./findAnagrams')
-const makeRegexForSubwords = require('./subwordsRegex')
 const flatten = require('flat')
 
 // Defining all methods and logic for routes
@@ -60,14 +59,9 @@ module.exports = {
 	},
 	findSubwords: function(req,res) {
 		let input = req.query.input
-		Word.find({"Length": {
-					"$lte": input.length
-					// "$regex": makeRegexForSubwords(input)
-				}
-			})
+		Word.find({"Length": {"$lte": input.length}})
 			.select({"Word": 1, "NoMacraLowerCase": 1, "NoMacra": 1, "Length": 1, "_id": 0})
 			.then(words=>{
-				// let sortedSubwords = words;
 				let sortedSubwords = findSubwordsFromMongo(input,words)
 				let subwordsOnlyWord = sortedSubwords.map((object)=>{
 					return object.Word
