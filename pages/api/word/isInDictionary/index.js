66c37703f72290cbfa87467e540286cdc3f0e44e
DuncanRitchie <duncanritchie@btinternet.com>
2022-05-29 11:15:22 +0100

    Deleted commented-out code from API func

--- pages/api/word/isInDictionary/index.js
+++ pages/api/word/isInDictionary/index.js
@@ -3,13 +3,4 @@ import { findOneWordSelectOnlyWord } from "../../../../lib/words/word";
 export default async function findOneWord(req, res) {
     const foundWord = await findOneWordSelectOnlyWord(req.query.word);
     res.send(foundWord);
-    // if (foundWord.success) {
-    //     const subwordsAsWords = foundWord.subwords.map((object)=>{
-    //         return object.Word
-    //     })
-    //     return { success: true, subwords: subwordsAsWords }
-    // }
-    // else {
-    //     return foundWord
-    // }
 }
