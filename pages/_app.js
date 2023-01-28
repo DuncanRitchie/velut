9723b37ddd10e778e2a61b4bc4f3f65014784a83
DuncanRitchie <duncanritchie@btinternet.com>
2023-01-28 20:06:48 +0000

    Fixed an import for production
    Apparently imports are not case-sensitive in my dev environment but in prod they are!

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import DefaultHead from '../components/defaultHead/DefaultHead'
 import Footer from '../components/footer/Footer'
-import TemporaryWarning from '../components/temporaryWarning/temporaryWarning'
+import TemporaryWarning from '../components/temporaryWarning/TemporaryWarning'
 import styles from '../css/globals.css'
 
 function App({ Component, pageProps }) {
