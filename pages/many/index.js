16d14f617fad95ed53ea08d6dbd7e63baece9a80
DuncanRitchie <duncanritchie@btinternet.com>
2023-03-11 18:44:00 +0000

    Fixed query in redirect from /many to /multiword

--- pages/many/index.js
+++ pages/many/index.js
@@ -1,11 +1,11 @@
 // `RedirectFromManyPage` is triggered from /many to redirect to /multiword.
+// The query string is preserved across the redirect.
 function RedirectFromManyPage() {}
 
 export const getServerSideProps = async ({ query, res }) => {
+  const queryParams = new URLSearchParams(query)
   res.writeHead(301, {
-    Location: `/multiword?search=${query.search}${
-      query.ssr == 'true' ? '&ssr=true' : ''
-    }`,
+    Location: `/multiword?${queryParams.toString()}`,
   })
   res.end()
   return { props: {} }
