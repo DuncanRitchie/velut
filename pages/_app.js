f6e9ac3fd52a7404009155dde1d033e3f0206a2d
DuncanRitchie <duncanritchie@btinternet.com>
2024-04-29 21:41:52 +0100

    Deleted temporary warning

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import DefaultHead from '../components/defaultHead/DefaultHead'
 import Footer from '../components/footer/Footer'
-import TemporaryWarning from '../components/temporaryWarning/TemporaryWarning'
 import styles from '../css/globals.css'
 
 function App({ Component, pageProps }) {
   return (
     <>
       <DefaultHead />
-      <TemporaryWarning />
       <Component {...pageProps} />
       <Footer />
     </>
