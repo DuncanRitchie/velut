f9572bb0c69d4a7ec0b70cfd027d75a84f06735f
DuncanRitchie <duncanritchie@btinternet.com>
2024-09-23 15:55:15 +0100

    Wrapped Search in ErrorBoundary

--- components/search/Search.jsx
+++ components/search/Search.jsx
@@ -3,6 +3,7 @@ import { Component } from 'react'
 import routes from '../../data/routes.json'
 import urlFromSearch from '../../lib/urlFromSearch'
 import styles from './Search.module.css'
+import ErrorBoundary from '../errorBoundary/errorBoundary'
 
 class Search extends Component {
   constructor(props) {
@@ -61,57 +62,59 @@ class Search extends Component {
       })
     // Now we’re ready to return JSX.
     return (
-      // If client doesn’t have JavaScript, `action` & `method` are used.
-      // If client has JavaScript, the `onSubmit` is used.
-      <form
-        className={styles.search}
-        action="/redirectonsearch"
-        method="get"
-        onSubmit={this.handleSubmit}
-        role="search"
-      >
-        <label htmlFor="search-input" className="visually-hidden">
-          {this.props.searchbarLabel || 'Text to search for'}
-        </label>
-        {/* The box the word will be typed into. */}
-        <input
-          id="search-input"
-          name="word"
-          value={this.state.word || ''}
-          onChange={this.handleInput}
-          onKeyUp={this.handleInputKeyUp}
-          title={this.props.searchbarLabel || 'Text to search for'}
-          type="text"
-          lang={this.props.lang || 'la'}
-          autoCapitalize="none"
-          autoComplete="off"
-          autoCorrect="off"
-          spellCheck="false"
-          enterKeyHint="search"
-        />
+      <ErrorBoundary>
+        {/* If client doesn’t have JavaScript, `action` & `method` are used.
+        If client has JavaScript, the `onSubmit` is used. */}
+        <form
+          className={styles.search}
+          action="/redirectonsearch"
+          method="get"
+          onSubmit={this.handleSubmit}
+          role="search"
+        >
+          <label htmlFor="search-input" className="visually-hidden">
+            {this.props.searchbarLabel || 'Text to search for'}
+          </label>
+          {/* The box the word will be typed into. */}
+          <input
+            id="search-input"
+            name="word"
+            value={this.state.word || ''}
+            onChange={this.handleInput}
+            onKeyUp={this.handleInputKeyUp}
+            title={this.props.searchbarLabel || 'Text to search for'}
+            type="text"
+            lang={this.props.lang || 'la'}
+            autoCapitalize="none"
+            autoComplete="off"
+            autoCorrect="off"
+            spellCheck="false"
+            enterKeyHint="search"
+          />
 
-        {/* The `type` parameter determines the type of results displayed on the page.
+          {/* The `type` parameter determines the type of results displayed on the page.
                 If the dropdown menu should appear on the page, it will set `type`.
                 Otherwise, `type` will come from a hidden <input> element.
                 The latter happens if the path begins with /about, /english, /subwords, or /anagramphrases. */}
-        {this.props.hideDropdown ? (
-          <input name="type" type="hidden" value={this.state.type}></input>
-        ) : (
-          <div className={styles.dropdown + ' with-dropdown-arrow'}>
-            <label htmlFor="rhyme-type-dropdown" className="visually-hidden">
-              Type of rhymes to include
-            </label>
-            <select name="type" id="rhyme-type-dropdown" value={this.state.type} onChange={this.handleType}>
-              {dropdownOptions}
-            </select>
-          </div>
-        )}
+          {this.props.hideDropdown ? (
+            <input name="type" type="hidden" value={this.state.type}></input>
+          ) : (
+            <div className={styles.dropdown + ' with-dropdown-arrow'}>
+              <label htmlFor="rhyme-type-dropdown" className="visually-hidden">
+                Type of rhymes to include
+              </label>
+              <select name="type" id="rhyme-type-dropdown" value={this.state.type} onChange={this.handleType}>
+                {dropdownOptions}
+              </select>
+            </div>
+          )}
 
-        {/* The button to load the new page. */}
-        <button className={styles.searchButton} type="submit">
-          Search!
-        </button>
-      </form>
+          {/* The button to load the new page. */}
+          <button className={styles.searchButton} type="submit">
+            Search!
+          </button>
+        </form>
+      </ErrorBoundary>
     )
   }
 }
