0146df2ef8220b0ef5967010bef06016cde283a7
DuncanRitchie <duncanritchie@btinternet.com>
2024-10-17 20:13:15 +0100

    Removed “not verified” for lemmata in ParsingsList where forms have been checked

--- models/Lemma.js
+++ models/Lemma.js
@@ -16,6 +16,15 @@ const lemmaSchema = new mongoose.Schema(
     NoMacra: String,
     NoMacraLowerCase: String,
     Forms: Object,
+    // Whether generated forms (`Forms`) are displayed depends on the
+    // NEXT_PUBLIC_SHOW_GENERATED_FORMS_FOR env var, which lists the
+    // parts of speech for which all forms have been checked.
+    // But for parts of speech where some lemmata have had their forms checked
+    // and some haven’t, it’s useful to have a property distinguishing those lemmata.
+    // This is temporary while I’m checking forms — eventually all parts of speech
+    // will be in the env var and all lemmata will have `FormsHaveBeenChecked`
+    // equal to true, making both the env var and the property unnecessary.
+    FormsHaveBeenChecked: Boolean,
   },
   { collection: 'lemmata' },
 )
