dd3826e7c291412332996f87e98718564ebc4afc
DuncanRitchie <duncanritchie@btinternet.com>
2025-07-07 21:27:01 +0100

    Added a page for checking app health

---
+++ pages/healthcheck.js
@@ -0,0 +1,7 @@
+// Pinging /healthcheck gives an idea of whether the application is working,
+// without wasting time generating a page like the pages users are expected to see.
+const HealthCheck = () => {
+  return <p>This page is working correctly!</p>
+}
+
+export default HealthCheck
