3f273426a5afa1eaa593492ae87c6af5709815ba
DuncanRitchie <duncanritchie@btinternet.com>
2025-10-26 20:29:41 +0000

    data-scroll-behavior attribute like Next.js wants

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,12 @@
-//// This file exists to give <html> a `lang` attribute.
+//// This file exists to give <html> a `lang` attribute
+//// and because Next.js seems to want a data-scroll-behavior attribute
+//// https://nextjs.org/docs/messages/missing-data-scroll-behavior
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" data-scroll-behavior="smooth">
         <Head />
         <body>
           <Main />
