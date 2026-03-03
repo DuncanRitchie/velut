7c767780b1649152b0e4a1e5d2358deefa882015
DuncanRitchie <duncanritchie@btinternet.com>
2026-03-03 20:10:41 +0000

    Disabled prefetching on <LatinLink> (I don’t mind prefetching for links outside of <LatinLink>.)

--- components/latinlink/LatinLink.jsx
+++ components/latinlink/LatinLink.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import { macraToHyphens } from '../../lib/words/diacritics'
 import superscriptLemmaTag from '../lemma/superscriptLemmaTag'
 
-const LatinLink = ({ linkBase = '/', targetWord, currentWordHyphenated, isLemma = false }) => {
+const LatinLink = ({ linkBase = '/', targetWord, currentWordHyphenated, isLemma = false, prefetch = false }) => {
   const text = isLemma ? superscriptLemmaTag(targetWord) : targetWord
   const targetWordHyphenated = macraToHyphens(targetWord)
   const to = isLemma ? targetWordHyphenated.replace(/\[[^\]]*\]/g, '') : targetWordHyphenated
@@ -11,7 +11,11 @@ const LatinLink = ({ linkBase = '/', targetWord, currentWordHyphenated, isLemma 
 
   if (shouldDisplayLink) {
     return (
-      <Link href={linkBase + to} lang="la">
+      // Next.js <Link> has prefetch enabled by default, but for velut I have it disabled by default.
+      // Prefetching would mean that requests are made for every Latin word in the viewport,
+      // before any links were actually clicked, which is wasteful on pages with a lot of Latin words linked.
+      // See https://mikebifulco.com/posts/reduce-nextjs-bandwidth-with-link-prefetch
+      <Link href={linkBase + to} lang="la" prefetch={prefetch}>
         {text}
       </Link>
     )
