4a3b0ae50570ea38bca741bd107065c9f22bd61f
DuncanRitchie <duncanritchie@btinternet.com>
2026-03-06 21:39:22 +0000

    Root property on lemma is gone (all lemmata have Roots array now, not singular Root)

--- models/Lemma.js
+++ models/Lemma.js
@@ -1,11 +1,6 @@
 import mongoose from 'mongoose'
 
 const lemmaSchema = new mongoose.Schema(
-  // Both Root and Roots are used in velut.
-  // Root is the older field, used for specifying one lemma that is an etymological root of the lemma in the object.
-  // Roots is a newer field, used for specifying at least one root.
-  // Code for finding cognates considers all the roots in Roots, or simply Root (if there is no Roots).
-  // My intention over time is that I will fill out all lemmata with a Roots field, and then I’ll deprecate Root.
   {
     // Ord: Number,
     Word: String,
@@ -15,7 +10,6 @@ const lemmaSchema = new mongoose.Schema(
     Transliterations: String,
     // Scansion: String,
     // SyllableCount: Number,
-    Root: String,
     Roots: [String],
     // FormCount: Number,
     // NoTypeTag: String,
